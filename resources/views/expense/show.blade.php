627669e87c87439117bb40e7b18a90861ca35b69
Layson Ndenza <laysonndenza@gmail.com>
2026-01-24 13:09:56 +0300

    new updates

--- resources/views/expense/show.blade.php
+++ resources/views/expense/show.blade.php
@@ -33,15 +33,15 @@
                     </div>
                 </div>
                 <div class="form-group row">
-                    <label class="col-md-4 col-form-label text-md-right">Updated by:</label>
-                    <div class="col-md-8">
-                        <input type="text" class="form-control" id="show_updated_by" readonly>
-                    </div>
-                </div>
-                <div class="form-group row">
                     <label class="col-md-4 col-form-label text-md-right">Description:</label>
                     <div class="col-md-8">
                         <input class="form-control" id="show_description" readonly rows="3"></input>
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label class="col-md-4 col-form-label text-md-right">Updated by:</label>
+                    <div class="col-md-8">
+                        <input type="text" class="form-control" id="show_updated_by" readonly>
                     </div>
                 </div>
             </div>
